e83cd82398511554492f927e5d4d493191435c94
tpsumeta <tpsumeta@gmail.com>
2020-03-10 17:45:26 +0700

    Init

---
+++ src/main/java/io/github/sumeta/springboot/Springboot2PaypalApplication.java
@@ -0,0 +1,13 @@
+package io.github.sumeta.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2PaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2PaypalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sumeta/springboot/Springboot2PaypalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sumeta.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2PaypalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
